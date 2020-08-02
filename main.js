19ddbe805ddf5ebc9e99f10a3e5786f8b2f7cb4d
simpsonyuki <yukilin0327@gmail.com>
2020-08-02 13:00:59 +0800

    完成

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 new Vue ({
-    el: "#restraunt",
+    el: "#restaurant",
     data: {
         datas: null //空值
     },
+
+    // 顯示所有元素後執行
     mounted() {
-        
+        var xhr = new XMLHttpRequest;
+        xhr.open("GET", "https://datacenter.taichung.gov.tw/swagger/OpenData/72b2e32b-74e4-4000-b920-7457c54565be");
+        xhr.send();
+
+        xhr.onload = () => {
+            var json = JSON.parse(xhr.responseText);
+            this.datas = json;
+        }
+
     },
 });
+
